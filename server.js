e5760f84a69399ef7aa7b80c143118cdbca3ba6d
Youngjun Chang <vitakcrete@gmail.com>
2022-04-14 18:56:42 +0900

    master commit

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const express = require("express")
-const path = require("path")
 const app = express();
-const server = require("http").createServer(app)
+const path = require("path");
+const { client } = require("websocket");
+const url = require("url")
 
+const server = require("http").createServer(app)
+const websocket = require("ws") 
 app.use(express.static(path.join(__dirname + "/src"))) ;
 const PORT = process.env.PORT || 8080 ;
+
 
 
 app.listen(PORT, function(){
     console.log(`지금 연결된 포트는 ${PORT}번 입니다.`)
 })
+
+const wst = require("ws")
+const wss = new wst.Server({port:8001})
+var connects = []
+
+wss.on("connection", function(ws,req) {
+    var location = url.parse(req.url, true);
+
+    var initMessage = {message:"connection"};
+    ws.send(JSON.stringify(initMessage));
+    connects.push(ws);
+    console.log("New Client Connected : " + connects.length);
+
+    ws.on("message", data =>{
+        
+        console.log(`Received from user: ${data}`)
+        if (data="allOn"){
+            var msg = 1
+            ws.send(msg)
+            broadcast(msg);
+        }
+      //  var msg = 1
+      //  ws.send(msg)
+      //  broadcast(msg);
+        
+          // Return to client
+    }) 
+})
+
+function broadcast (data) {
+    connects.forEach(function (anything, i) {
+        anything.send(data);
+    });
+}
